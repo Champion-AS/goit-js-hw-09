5296e617890e6b54944de4082903596efdc440d2
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-15 17:13:14 +0300

    1 task
    Змінює бекраунд при натисканні на старт, зупиняє рандом кольорів при натисканні на стоп

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+btnStop.disabled = true;
+let timerId;
+
+btnStart.addEventListener('click', onStart);
+
+function onStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        bodyEl.style.background = color;
+}, 1000)
+}
+
+btnStop.addEventListener('click', onStop);
+
+function onStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+}
 
