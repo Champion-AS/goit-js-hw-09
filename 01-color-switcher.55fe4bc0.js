4a00656d113b0f376417faf2935926736447a665
Champion-AS <108135994+Champion-AS@users.noreply.github.com>
2022-09-20 14:57:26 +0000

    Deploying to gh-pages from @ Champion-AS/goit-js-hw-09@92b0e8184ab9fdf1f0d1bfe44554595b3889e989 🚀

---
+++ 01-color-switcher.55fe4bc0.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.disabled,n.disabled;e.addEventListener("click",(function(){!0,!1,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.background=t}),1e3)})),n.addEventListener("click",(function(){!1,!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.55fe4bc0.js.map
